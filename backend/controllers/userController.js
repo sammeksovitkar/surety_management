258dbff0b008104ebb07c26fe90b739baa8e6454
sammeksovitkar <83156211+sammeksovitkar@users.noreply.github.com>
2025-11-10 11:35:14 +0530

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -335,3 +335,16 @@ exports.getAllHardware = async (req, res) => {
         res.status(500).json({ msg: 'Server error' });
     }
 };
+exports.getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        if (!user) {
+            return res.status(404).json({ msg: 'User not found' });
+        }
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ msg: 'Server error' });
+    }
+};
+
